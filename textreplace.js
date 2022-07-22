9fb714b7a98edb7e2834236be806b9b0121e185f
Tony <tyin363@aucklanduni.ac.nz>
2022-07-23 11:26:26 +1200

    Changed js files

--- textreplace.js
+++ textreplace.js
@@ -4,6 +4,6 @@ const text = document.querySelectorAll(
 
 for (let i = 0; i < text.length; i++) {
   if (text[i].innerHTML.includes("")) {
-    text[i].innerHTML = text[i].innerHTML.replace("", "honk");
+    text[i].innerHTML = text[i].innerHTML.replace("For", "honk");
   }
 }
