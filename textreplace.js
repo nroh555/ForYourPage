e0f0ab935ba7d0a6007c24474147c39cb578b298
Tony <tyin363@aucklanduni.ac.nz>
2022-07-23 11:23:11 +1200

    Changed textreplace

--- textreplace.js
+++ textreplace.js
@@ -1,7 +1,9 @@
-var texts = document.querySelectorAll('h1, h2, h33, h4, h5, p, li, td, caption, span, a');
+const text = document.querySelectorAll(
+  "h1, h2, h3, h4, h5, p, li, td, caption, span, a"
+);
 
-for (var i = 0; i < texts.length; i++){
-    if (text[i].innerHTML.includes(' ')){
-        text[i].innerHTML = text[i].innerHTML.replace(' ', ' honk ')
-    }
+for (let i = 0; i < text.length; i++) {
+  if (text[i].innerHTML.includes("")) {
+    text[i].innerHTML = text[i].innerHTML.replace("", "honk");
+  }
 }
