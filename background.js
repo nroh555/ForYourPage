b5defe7fb7863f6ff1059efdab56941c8f0c506b
Kai <khir829@aucklanduni.ac.nz>
2022-07-23 17:24:52 +1200

    Add text replace file

--- background.js
+++ background.js
@@ -14,14 +14,30 @@ function textReplacer() {
 
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
-        
     
         for (var j = 0; j < element.childNodes.length; j++) {
             var node = element.childNodes[j];
     
             if (node.nodeType === 3) {
                 var text = node.nodeValue;
-                var replacedText = text.replace(/Football/gi, 'Basketball');
+                var replacedText = text.replace('.', ' on gawd. ');
+    
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
+            }
+        }
+    }
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+
+        for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
+    
+            if (node.nodeType === 3) {
+                var text = node.nodeValue;
+                var replacedText = text.replace('the', 'da');
     
                 if (replacedText !== text) {
                     element.replaceChild(document.createTextNode(replacedText), node);
