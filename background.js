9189b596a12dffc4148967120ba9fd2c3cf18f3c
Naren <nroh555@aucklanduni.ac.nz>
2022-07-24 15:51:11 +1200

    Added joke notification

--- background.js
+++ background.js
@@ -93,3 +93,43 @@ function textReplacer() {
 // document.getElementById("rick").onclick = function () {
 //     location.href = "https://www.youtube.com/";
 // };
+
+var puns = ["Two artist had an art contest. It ended in a draw", "Why do we tell actors to break a leg? Because every play has a cast", "Why did the fish get high? Because it ate seaweed", "Why was the stadium so cold? Cause it had so many fans", "Rabbits love hip-hop.", "What did the buffalo say when his son left home? Bison"];
+
+chrome.alarms.onAlarm.addListener(
+    () => {
+        random = Math.floor(Math.random() * 10); // 0 - 9
+        chrome.notifications.create(
+            // "drink_water",
+            {
+                type: "basic",
+                iconUrl: "emoji-icon.png",
+                title: "You got a joke",
+                message: puns[random],
+                silent: false
+            },
+            () => { }
+        )
+    },
+)
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(request);
+        if (request.time)
+            createAlarm();
+
+        sendResponse(() => {
+            return false
+        });
+    }
+);
+
+function createAlarm() {
+    chrome.alarms.create(
+        "have_fun",
+        {
+            delayInMinutes: 0,
+            periodInMinutes: 1
+        }
+    );
+}
---
+++ content.js
@@ -0,0 +1,6 @@
+const ele = document.getElementById("btn")
+ele.addEventListener("click", () => {
+    chrome.runtime.sendMessage({ time: "1" }, function (response) {
+        console.log(response);
+    });
+});
