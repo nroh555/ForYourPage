6f43e4f6cf6eb26ebb92f3e0504980290c55d4f4
lukekimchi <lukekim117@gmail.com>
2022-07-24 12:43:28 +1200

    Motivational speech added

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+// Rick roll
 document.addEventListener('DOMContentLoaded', () => {
     var y = document.getElementById("rick");
     y.addEventListener("click", openIndex);
@@ -6,6 +7,28 @@ document.addEventListener('DOMContentLoaded', () => {
  function openIndex() {
   chrome.tabs.create({active: true, url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"});
  }
+
+// Motivational speech 1 - there is no passion
+document.addEventListener('DOMContentLoaded', () => {
+    var y = document.getElementById("btnSpeech1");
+    y.addEventListener("click", playSpeech1);
+ });
+ 
+ function playSpeech1() {
+  chrome.tabs.create({active: true, url: "https://www.youtube.com/watch?v=syPxMG-o0as"});
+ }
+
+
+ // Yeah! 
+ document.addEventListener('DOMContentLoaded', () => {
+    var btn = document.getElementById("btnYeah");
+    btn.addEventListener("click", playYeah);
+ });
+var audio = new Audio("./sounds/yeah.mp3");
+function playYeah() {
+    console.log("test");
+    audio.play();
+}
 
 
  
@@ -18,13 +41,5 @@ document.addEventListener('DOMContentLoaded', () => {
     alert("Hello..! It's my first chrome extension.");
  }
 
-document.addEventListener('DOMContentLoaded', () => {
-    var btn = document.getElementById("btnYeah");
-    btn.addEventListener("click", playYeah);
- });
-var audio = new Audio("./sounds/yeah.mp3");
-function playYeah() {
-    console.log("test");
-    audio.play();
-}
+
 
